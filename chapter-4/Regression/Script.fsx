09a46cd835a452d4e9dbcbc4b4f11a9ef2f0d3b0
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2015-06-23 15:22:37 +0200

    Chapter 4 code

---
+++ chapter-4/Regression/Script.fsx
@@ -0,0 +1,164 @@
+﻿#I @"packages\"
+#r @"FSharp.Data.2.2.1\lib\net40\FSharp.Data.dll"
+
+open FSharp.Data
+
+type Data = CsvProvider<"day.csv">
+let dataset = Data.Load("day.csv")
+let data = dataset.Rows
+
+// basic statistics
+
+let count = 
+    data |> Seq.map (fun x -> float x.Cnt)
+
+let avg = count |> Seq.average
+let min = count |> Seq.min
+let max = count |> Seq.max
+
+let baseline = 
+    let avg = data |> Seq.averageBy (fun x -> float x.Cnt)
+    data |> Seq.averageBy (fun x -> abs (float x.Cnt - avg))
+
+// Basic plots
+#load @"FSharp.Charting.0.90.10\FSharp.Charting.fsx"
+open FSharp.Charting
+
+let all = Chart.Line [ for obs in data -> obs.Cnt ]
+
+// combine 2 line charts
+let detail = Chart.Combine [
+    Chart.Line [ for obs in data -> obs.Casual ]
+    Chart.Line [ for obs in data -> obs.Registered ] ]
+
+// scatterplot of registered vs casual users
+let reg_vs_cas = 
+    [ for obs in data -> obs.Registered, obs.Casual ] 
+    |> Chart.Point
+
+// denoising with moving average
+
+let windowedExample =
+    [ 1 .. 10 ]
+    |> Seq.windowed 3
+    |> Seq.toList
+
+let ma n (series:float seq) = 
+    series 
+    |> Seq.windowed n
+    |> Seq.map (fun xs -> xs |> Seq.average)
+    |> Seq.toList
+
+Chart.Combine [
+    Chart.Line count
+    Chart.Line (ma 7 count)
+    Chart.Line (ma 30 count) ]
+
+(*
+Basic model: fitting a straight line
+*)
+
+type Obs = Data.Row
+
+let model (theta0, theta1) (obs:Obs) = 
+    theta0 + theta1 * (float obs.Instant)
+
+let model0 = model (4504., 0.)
+let model1 = model (6000., -4.5)
+
+Chart.Combine [
+    Chart.Line count
+    Chart.Line [ for obs in data -> model0 obs ]
+    Chart.Line [ for obs in data -> model1 obs ] ]
+
+type Model = Obs -> float
+
+let cost (data:Obs seq) (m:Model) =
+    data 
+    |> Seq.sumBy (fun x -> pown (float x.Cnt - m x) 2)
+    |> sqrt
+
+let overallCost = cost data
+overallCost model0 |> printfn "Cost model0: %.0f" 
+overallCost model1 |> printfn "Cost model1: %.0f" 
+
+(*
+Gradient descent
+*)
+
+let update alpha (theta0, theta1) (obs:Obs) =
+    let y = float obs.Cnt
+    let x = float obs.Instant
+    let theta0' = theta0 - 2. * alpha * 1. * (theta0 + theta1 * x - y)
+    let theta1' = theta1 - 2. * alpha * x *  (theta0 + theta1 * x - y)
+    theta0', theta1'
+
+let obs100 = data |> Seq.nth 100
+let testUpdate = update 0.00001 (0.,0.) obs100
+cost [obs100] (model (0.,0.))
+cost [obs100] (model testUpdate)
+
+let stochastic rate (theta0,theta1) = 
+    data 
+    |> Seq.fold (fun (t0,t1) obs -> 
+        printfn "%.4f,%.4f" t0 t1
+        update rate (t0,t1) obs) (theta0,theta1)
+
+let tune_rate = 
+    [ for r in 1 .. 20 -> 
+        (pown 0.1 r), stochastic (pown 0.1 r) (0.0,0.0) |> model |> overallCost ]
+
+let rate = pown 0.1 8
+let model2 = model (stochastic rate (0.0,0.0))
+
+Chart.Combine [
+    Chart.Line count
+    Chart.Line [ for obs in data -> model2 obs ] ]
+
+
+overallCost model2
+overallCost model1
+
+let hiRate = 10.0 * rate
+let error_eval =
+    data 
+    |> Seq.scan (fun (t0,t1) obs -> update hiRate (t0,t1) obs) (0.0,0.0)
+    |> Seq.map (model >> overallCost)
+    |> Chart.Line
+
+let batchUpdate rate (theta0, theta1) (data:Obs seq) =
+    let updates = 
+        data 
+        |> Seq.map (update rate (theta0, theta1))
+    let theta0' = updates |> Seq.averageBy fst
+    let theta1' = updates |> Seq.averageBy snd    
+    theta0', theta1'
+
+let batch rate iters =
+    let rec search (t0,t1) i =
+        if i = 0 then (t0,t1)
+        else
+            search (batchUpdate rate (t0,t1) data) (i-1)
+    search (0.0,0.0) iters
+
+[ for r in 1 .. 10 -> batch (pown 0.1 r) 100 |> model |> overallCost ]
+
+let better = model (batch (pown 0.1 6) 10000)
+overallCost better
+overallCost model1
+overallCost model2
+
+Chart.Combine [
+    Chart.Line count
+    Chart.Line [ for obs in data -> better obs ] ]
+
+let batched_error rate =
+    Seq.unfold (fun (t0,t1) -> 
+        let (t0',t1') = batchUpdate rate (t0,t1) data
+        let err = model (t0,t1) |> overallCost
+        Some(err, (t0',t1'))) (0.0,0.0)
+    |> Seq.take 100
+    |> Seq.toList
+    |> Chart.Line
+
+batched_error 0.000001
---
+++ chapter-4/Regression/algebra.fsx
@@ -0,0 +1,229 @@
+﻿#I @"packages\"
+#r @"FSharp.Data.2.2.1\lib\net40\FSharp.Data.dll"
+#load @"FSharp.Charting.0.90.10\FSharp.Charting.fsx"
+#r @"MathNet.Numerics.Signed.3.6.0\lib\net40\MathNet.Numerics.dll"
+#r @"MathNet.Numerics.FSharp.Signed.3.6.0\lib\net40\MathNet.Numerics.FSharp.dll"
+
+open FSharp.Charting
+open FSharp.Data
+open MathNet
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.LinearAlgebra.Double
+
+(*
+Validate that algebra model is doing
+the same thing as the previous model
+*)
+
+
+let A = vector [ 1.; 2.; 3. ]
+let B = matrix [ [ 1.; 2. ]
+                 [ 3.; 4. ]
+                 [ 5.; 6. ] ]
+
+let C = A * A
+let D = A * B
+let E = A * B.Column(1)
+
+type Data = CsvProvider<"day.csv">
+let dataset = Data.Load("day.csv")
+let data = dataset.Rows
+
+type Vec = Vector<float>
+type Mat = Matrix<float>
+
+let cost (theta:Vec) (Y:Vec) (X:Mat) =
+    let ps = Y - (theta * X.Transpose())
+    ps * ps |> sqrt
+
+let predict (theta:Vec) (v:Vec) = theta * v 
+
+let X = matrix [ for obs in data -> [ 1.; float obs.Instant ]]
+let Y = vector [ for obs in data -> float obs.Cnt ]
+
+let theta = vector [6000.; -4.5]
+
+predict theta (X.Row(0))
+cost theta Y X 
+
+#time
+let theta_normal = (X.Transpose() * X).Inverse() * X.Transpose() * Y
+
+(*
+Adding MKL provider for faster linear algebra
+*)
+
+System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+open MathNet.Numerics
+open MathNet.Numerics.Providers.LinearAlgebra.Mkl
+Control.LinearAlgebraProvider <- MklLinearAlgebraProvider()
+
+(*
+Preparing for cross validation
+*)
+
+let seed = 314159
+let rng = System.Random(seed)
+
+// Fischer-Yates shuffle
+let shuffle (arr:'a []) =
+    let arr = Array.copy arr
+    let l = arr.Length
+    for i in (l-1) .. -1 .. 1 do
+        let temp = arr.[i]
+        let j = rng.Next(0,i+1)
+        arr.[i] <- arr.[j]
+        arr.[j] <- temp
+    arr
+
+let myArray = [| 1 .. 5 |]
+myArray |> shuffle
+
+let training,validation =
+    let shuffled =
+        data
+        |> Seq.toArray
+        |> shuffle
+    let size = 
+        0.7 * float (Array.length shuffled) |> int
+    shuffled.[..size],
+    shuffled.[size+1..]
+
+(* 
+Using normal form for estimation
+*)
+
+let estimate (Y:Vec) (X:Mat) =
+    (X.Transpose() * X).Inverse() * X.Transpose() * Y
+
+let thetaCompare = estimate Y X
+
+type Obs = Data.Row
+type Model = Obs -> float
+type Featurizer = Obs -> float list
+
+let predictor (f:Featurizer) (theta:Vec) =
+    f >> vector >> (*) theta
+
+let evaluate (model:Model) (data:Obs seq) =
+    data 
+    |> Seq.averageBy (fun obs -> 
+        abs (model obs - float obs.Cnt))
+
+let model (f:Featurizer) (data:Obs seq) =
+    let Yt, Xt = 
+        data
+        |> Seq.toList
+        |> List.map (fun obs -> float obs.Cnt, f obs)
+        |> List.unzip
+    let theta = estimate (vector Yt) (matrix Xt)
+    let predict = predictor f theta
+    theta,predict
+
+let featurizer0 (obs:Obs) = 
+    [   1.; 
+        float obs.Instant; ]
+
+let (theta0,model0) = model featurizer0 training
+
+Chart.Combine [
+    Chart.Line [ for obs in data -> float obs.Cnt ]
+    Chart.Line [ for obs in data -> model0 obs ] ]
+
+evaluate model0 training |> printfn "Training: %.0f"
+evaluate model0 validation |> printfn "Validation: %.0f"
+
+(*
+Expanded model
+*)
+
+let featurizer1 (obs:Obs) =
+    [   1.
+        obs.Instant |> float
+        obs.Atemp |> float
+        obs.Hum |> float
+        obs.Temp |> float
+        obs.Windspeed |> float
+    ]
+
+let (theta1,model1) = model featurizer1 training
+
+evaluate model1 training |> printfn "Training: %.0f"
+evaluate model1 validation |> printfn "Validation: %.0f"
+
+Chart.Combine [
+    Chart.Line [ for obs in data -> float obs.Cnt ]
+    Chart.Line [ for obs in data -> model0 obs ]
+    Chart.Line [ for obs in data -> model1 obs ] ]
+
+Chart.Point [ for obs in data -> float obs.Cnt, model1 obs ]
+
+
+(*
+Transforming categoricals into features
+*)
+
+let featurizer2 (obs:Obs) =
+    [   1.
+        obs.Instant |> float
+        obs.Hum |> float
+        obs.Temp |> float
+        obs.Windspeed |> float
+        (if obs.Weekday = 1 then 1.0 else 0.0)
+        (if obs.Weekday = 2 then 1.0 else 0.0)
+        (if obs.Weekday = 3 then 1.0 else 0.0)
+        (if obs.Weekday = 4 then 1.0 else 0.0)
+        (if obs.Weekday = 5 then 1.0 else 0.0)
+        (if obs.Weekday = 6 then 1.0 else 0.0)
+    ]
+
+let (theta2,model2) = model featurizer2 training
+
+evaluate model2 training |> printfn "Training: %.0f"
+evaluate model2 validation |> printfn "Validation: %.0f"
+
+(*
+Non linear features
+*)
+
+Chart.Point [ for obs in data -> obs.Temp, obs.Cnt ]
+
+let squareTempFeaturizer (obs:Obs) =
+    [   1.
+        obs.Temp |> float
+        obs.Temp * obs.Temp |> float ]
+
+let (_,squareTempModel) = model squareTempFeaturizer data
+
+Chart.Combine [
+    Chart.Point [ for obs in data -> obs.Temp, obs.Cnt ]
+    Chart.Point [ for obs in data -> obs.Temp, squareTempModel obs ] ]
+
+let featurizer3 (obs:Obs) =
+    [   1.
+        obs.Instant |> float
+        obs.Hum |> float
+        obs.Temp |> float
+        obs.Windspeed |> float
+        obs.Temp * obs.Temp |> float
+        (if obs.Weekday = 1 then 1.0 else 0.0)
+        (if obs.Weekday = 2 then 1.0 else 0.0)
+        (if obs.Weekday = 3 then 1.0 else 0.0)
+        (if obs.Weekday = 4 then 1.0 else 0.0)
+        (if obs.Weekday = 5 then 1.0 else 0.0)
+        (if obs.Weekday = 6 then 1.0 else 0.0)
+    ]
+
+let (theta3,model3) = model featurizer3 training
+
+evaluate model3 training |> printfn "Training: %.0f"
+evaluate model3 validation |> printfn "Validation: %.0f"
+
+Chart.Combine [
+    Chart.Line [ for obs in data -> float obs.Cnt ]
+    Chart.Line [ for obs in data -> model0 obs ]
+    Chart.Line [ for obs in data -> model3 obs ] ]
+
+Chart.Point [ for obs in data -> float obs.Cnt, model3 obs ]
+Chart.Point [ for obs in validation -> float obs.Cnt, model3 obs ]
